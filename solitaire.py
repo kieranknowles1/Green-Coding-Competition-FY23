4d66f0368a1c566b2813d7618dfc102c4f02ea2d
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-02 19:51:58 +0000

    Simplify array creation

--- solitaire.py
+++ solitaire.py
@@ -10,33 +10,9 @@ pp = pprint.PrettyPrinter(indent=4)
 with EmissionsTracker() as tracker:
 
     class Game:
-
-        x = "A"
-        y = "2"
-        z = "3"
-        a = "4"
-        b = "5"
-        c = "6"
-        d = "7"
-        e = "8"
-        f = "9"
-        g = "10"
-        h = "J"
-        i = "Q"
-        j = "K"
-
-        color1 = "red"
-        color2 = "black"
-
-        sign1 = "diamond"
-        sign2 = "spades"
-        sign3 = "hearts"
-        sign4 = "clubs"
-
-
-        values = [x, y, z, a, b, c, d, e, f, g, h, i, j]
-        colors = [color1, color2]
-        signs = [sign1, sign2, sign3, sign4]
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        colors = ["red", "black"]
+        signs = ["diamond", "spades", "hearts", "clubs"]
 
         list_of_values = values
 
@@ -46,17 +22,17 @@ with EmissionsTracker() as tracker:
                 for y in colors:
                     print("Card: " + i + " Color: " + y + " Symbol: " + x)
 
-            
-    
+
+
         suits = { #keys are unicode symbols for suits
             u'\u2660': "black",
             u'\u2665': "red",
             u'\u2663': "black",
             u'\u2666': "red",
         }
-    
+
         numPlayPiles = 7
-    
+
         def __init__(self):
             self.list_of_cards = [Card(value, suit) for value in range(1, 14) for suit in ["Diamonds", "Hearts", "Clubs", "Spades"]]
             self.deck = Deck(self.list_of_values,self.suits)
@@ -64,7 +40,7 @@ with EmissionsTracker() as tracker:
             for i in range(self.numPlayPiles):
                 thisPile = Pile()
                 [thisPile.addCard(self.deck.takeFirstCard(flip=False)) for j in range(i+1)]
-                thisPile.flipFirstCard()  
+                thisPile.flipFirstCard()
                 self.playPiles.append(thisPile)
             self.blockPiles = {suit: Pile() for suit in self.suits}
             self.deck.cards[0].flip()
@@ -76,18 +52,18 @@ with EmissionsTracker() as tracker:
                 "blockPiles": {suit: str(pile) for suit, pile in self.blockPiles.items()}
             }
             return returnObject
-        
+
         def checkCardOrder(self,higherCard,lowerCard):
             suitsDifferent = self.suits[higherCard.suit] != self.suits[lowerCard.suit]
             valueConsecutive = self.list_of_values[self.list_of_values.index(higherCard.value)-1] == lowerCard.value
             return suitsDifferent and valueConsecutive
-    
+
         def checkIfCompleted(self):
             deckEmpty = len(self.deck.cards)==0
             pilesEmpty = all(len(pile.cards)==0 for pile in self.playPiles)
             blocksFull = all(len(pile.cards)==13 for suit,pile in self.blockPiles.items())
             return deckEmpty and pilesEmpty and blocksFull
-            
+
         def addToBlock(self, card):
             if card is None:
                 return False
@@ -97,19 +73,19 @@ with EmissionsTracker() as tracker:
                     self.blockPiles[card.suit].cards.insert(0,card)
                     return True
                 else:
-                    return False   
-            else: 
+                    return False
+            else:
                 if card.value=="A":
                     self.blockPiles[card.suit].cards.insert(0,card)
                     return True
                 else:
                     return False
-        
+
         def takeTurn(self, verbose=False):
-                
+
             #Pre: flip up unflipped pile end cards -> do this automatically
             [pile.cards[0].flip() for pile in self.playPiles if len(pile.cards)>0 and not pile.cards[0].flipped]
-         
+
             #1: check if there are any play pile cards you can play to block piles
             for pile in self.playPiles:
                 if len(pile.cards) > 0 and self.addToBlock(pile.cards[0]):
@@ -119,14 +95,14 @@ with EmissionsTracker() as tracker:
                     return True
                 else:
                     print("Pile has cards")
-            
+
             #2: check if cards in deck can be added
             if self.addToBlock(self.deck.getFirstCard()):
                 card_added = self.deck.takeFirstCard()
                 if verbose:
                     print("Adding card from deck to block: {0}".format(str(card_added)))
                 return True
-            
+
             #3: move kings to open piles
             for pile in self.playPiles:
                 if len(pile.cards)==0: #pile has no cards
@@ -137,7 +113,7 @@ with EmissionsTracker() as tracker:
                             if verbose:
                                 print("Moving {0} from Pile to Empty Pile".format(str(card_added)))
                             return True
-                    
+
                     if self.deck.getFirstCard() is not None and self.deck.getFirstCard().value == "K":
                         card_added = self.deck.takeFirstCard()
                         pile.addCard(card_added)
@@ -146,19 +122,19 @@ with EmissionsTracker() as tracker:
                         return True
                 else:
                     print("Pile has cards")
-            
-            #4: add drawn card to playPiles 
+
+            #4: add drawn card to playPiles
             for pile in self.playPiles:
                 if len(pile.cards)>0 and self.deck.getFirstCard() is not None:
                     if self.checkCardOrder(pile.cards[0],self.deck.getFirstCard()):
                         card_added = self.deck.takeFirstCard()
-                        pile.addCard(card_added) 
+                        pile.addCard(card_added)
                         if verbose:
                             print("Moving {0} from Deck to Pile".format(str(card_added)))
                         return True
                 else:
                     print("Pile has cards")
-                            
+
             #5: move around cards in playPiles
             for pile1 in self.playPiles:
                 pile1_flipped_cards = pile1.getFlippedCards()
@@ -192,19 +168,19 @@ with EmissionsTracker() as tracker:
                 else:
                     print("Pile has cards")
             return False
-        
-                    
+
+
         def simulate(self, draw = False, verbose=False):
-        
+
             # clear cache if last turn was not card draw
             if not draw:
                 self.deck.cache = []
-        
+
             turnResult = self.takeTurn(verbose=verbose)
-        
+
             if turnResult:
                 self.simulate(verbose=verbose)
-                    
+
             else:
                 #End: draw from deck
                 if len(self.deck.cards)>0:
@@ -214,7 +190,7 @@ with EmissionsTracker() as tracker:
                     if currentCard in self.deck.cache:
                         if verbose:
                             print("No more moves left!")
-                        return 
+                        return
 
                     else:
                         self.deck.drawCard()
@@ -255,13 +231,5 @@ with EmissionsTracker() as tracker:
         sorted_cards = thisGame.bogosort()
         print("Sorted cards:", sorted_cards)
         return
-    
+
     main()
-
-
-
-
-
-
-
-
